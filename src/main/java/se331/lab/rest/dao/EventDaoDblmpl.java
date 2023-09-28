7c8ee663ca622ff2eb0bd730bbc42a5fa40e61ca
yanhang_yang <yanhang_y@cmu.ac.th>
2023-09-29 03:24:53 +0700

    1.9

--- src/main/java/se331/lab/rest/dao/EventDaoDblmpl.java
+++ src/main/java/se331/lab/rest/dao/EventDaoDblmpl.java
@@ -44,7 +44,7 @@ public class EventDaoDblmpl implements EventDao{
 
     @Override
     public Page<Event> getEvents(String title, Pageable page) {
-        return eventRepository.findByTitleContainingOrDescriptionContaining(title,title,page);
+        return eventRepository.findByTitleContainingOrDescriptionContainingOrOrganizer_NameContaining(title,title,title,page);
     }
 
 }
